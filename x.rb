db2a67acf36ccc7310967aa5a6cef295347f5b88
bambamboole <manuel@christlieb.eu>
2022-12-28 01:52:48 +0100

    Brew formula update for x version 0.1.0-rc10

--- x.rb
+++ x.rb
@@ -5,42 +5,42 @@
 class X < Formula
   desc "a simple and fast task runner"
   homepage "https://github.com/bambamboole/x"
-  version "0.1.0-rc9"
+  version "0.1.0-rc10"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/bambamboole/x/releases/download/0.1.0-rc9/x-0.1.0-rc9-darwin-arm64"
-      sha256 "39fd7c2c934fc59bd416c0ee58b026d9384bab5bd81a58d293aace26f7b9fde6"
+    if Hardware::CPU.intel?
+      url "https://github.com/bambamboole/x/releases/download/0.1.0-rc10/x-0.1.0-rc10-darwin-amd64"
+      sha256 "44840b46e0703549036ff4e86bfc5b14af21ce37f9b878c7e34317ca4a443139"
 
       def install
-        bin.install "x-0.1.0-rc9-darwin-arm64" => "x"
+        bin.install "x-0.1.0-rc10-darwin-amd64" => "x"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/bambamboole/x/releases/download/0.1.0-rc9/x-0.1.0-rc9-darwin-amd64"
-      sha256 "bd6fcd3913e647086536630df9b1b95cb1f98e21aca41d9fd240480dc044e79a"
+    if Hardware::CPU.arm?
+      url "https://github.com/bambamboole/x/releases/download/0.1.0-rc10/x-0.1.0-rc10-darwin-arm64"
+      sha256 "e2a8faedf03052d3c656a4d814b1691d413e04271bd2da1a28c4051e64e38c53"
 
       def install
-        bin.install "x-0.1.0-rc9-darwin-amd64" => "x"
+        bin.install "x-0.1.0-rc10-darwin-arm64" => "x"
       end
     end
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/bambamboole/x/releases/download/0.1.0-rc9/x-0.1.0-rc9-linux-arm64"
-      sha256 "34aeeed2395d051af18318c5a08185012b1e9e8e060ae8a800c1b49cd82728ad"
+    if Hardware::CPU.intel?
+      url "https://github.com/bambamboole/x/releases/download/0.1.0-rc10/x-0.1.0-rc10-linux-amd64"
+      sha256 "19a8e5d6d4b97564335827261a31396ec844dc0d6561f70f853fff41a3f888cd"
 
       def install
-        bin.install "x-0.1.0-rc9-linux-arm64" => "x"
+        bin.install "x-0.1.0-rc10-linux-amd64" => "x"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/bambamboole/x/releases/download/0.1.0-rc9/x-0.1.0-rc9-linux-amd64"
-      sha256 "e82826b20cfb82b570106057e77f5c32e3507182dab7077780e9ba2f0b13bffe"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/bambamboole/x/releases/download/0.1.0-rc10/x-0.1.0-rc10-linux-arm64"
+      sha256 "3c4bc65605a3361c95a485edc2c4e55c831aa48026313115815a1aa27dcdf676"
 
       def install
-        bin.install "x-0.1.0-rc9-linux-amd64" => "x"
+        bin.install "x-0.1.0-rc10-linux-arm64" => "x"
       end
     end
   end
